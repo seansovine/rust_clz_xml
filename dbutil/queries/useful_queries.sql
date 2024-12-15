c8a04547da1494f1a34522b6556470257088c226
Sean Sovine <sean.r.sovine@gmail.com>
2024-12-15 14:02:28 -0500

    Small updates to test query

--- dbutil/queries/useful_queries.sql
+++ dbutil/queries/useful_queries.sql
@@ -3,7 +3,7 @@
 -- Summary of book and author information.
 select b.id
      , b.title
-     , group_concat( a.last_name order by a.last_name separator ", ") as authors
+     , group_concat( a.last_name order by a.last_name separator ', ') as authors
 --      , a.first_name
      , b.isbn
      , b.publisher
@@ -11,8 +11,8 @@ select b.id
   join author a
   join author_book ab
  where ab.book_id = b.id and ab.author_id = a.id
- group by b.id
- order by b.title asc
+ group by b.id, b.title
+ order by b.title
 ;
 -- TODO: Add formatted first name to authors list.
 
